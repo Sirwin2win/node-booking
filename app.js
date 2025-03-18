3dc6d814889cbc44a503c91645694ff6d40db6b8
Sirwin <sirwin2win@gmail.com>
2025-03-18 04:06:17 +0100

    Static root change

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(cors())
 app.use(express.urlencoded({ extended: true }))
 app.use('/api/products', productRoute);
-app.use("/images", express.static('uploads'))
+app.use("/image", express.static('public'))
 
 
 app.use(express.static(path.join(__dirname, "build")));
